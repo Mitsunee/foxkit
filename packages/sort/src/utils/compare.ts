1bf8a9be8171d94eb7c8bae20ddcc73eb6a752a4
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-05-31 12:08:28 +0200

    fix(sort): compare giving wrong result for a equals b when using key as arg

--- packages/sort/src/utils/compare.ts
+++ packages/sort/src/utils/compare.ts
@@ -1,13 +1,17 @@
 interface CompareFn<T> {
   (a: T, b: T): number | boolean;
 }
+
 export type CompareArgument<T> = string | CompareFn<T>;
+
+// TODO: add tests
+
 // checks if a <= b
 export function compare<T>(a: any, b: any, arg?: CompareArgument<T>): boolean {
   switch (typeof arg) {
     case "string":
     case "number": {
-      return a[arg] < b[arg];
+      return a[arg] <= b[arg];
     }
     case "function": {
       const res = arg(a, b);
