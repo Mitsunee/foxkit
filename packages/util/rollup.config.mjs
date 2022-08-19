85c5784aea1cfa1176690652c684cebb2337ec56
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-08-19 10:43:10 +0200

    add(util): object module

--- packages/util/rollup.config.mjs
+++ packages/util/rollup.config.mjs
@@ -4,6 +4,7 @@ const makeConfig = initConfig();
 const config = [
   makeConfig({ input: "src/clamp/index.ts", key: "./clamp" }),
   makeConfig({ input: "src/dedent/index.ts", key: "./dedent" }),
+  makeConfig({ input: "src/object/index.ts", key: "./object" }),
   makeConfig({ input: "src/range/index.ts", key: "./range" }),
   makeConfig({ input: "src/sleep/index.ts", key: "./sleep" }),
   makeConfig({ input: "src/typeOf/index.ts", key: "./typeOf" })
