d58c3fdef67cad93d2f3d89786fef333b10ae3a6
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-08-23 16:47:01 +0200

    WIP: List

---
+++ packages/util/src/object/List.ts
@@ -0,0 +1,62 @@
+class ListNode<T> {
+  next: ListNode<T> | null = null;
+  prev: ListNode<T> | null = null;
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+export class List<T> {
+  #head: ListNode<T> | null = null;
+  #tail: ListNode<T> | null = null;
+  #length: number = 0;
+
+  get head() {
+    return this.#head;
+  }
+
+  get tail() {
+    return this.#tail;
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  // TODO: tests
+  push(value: T) {
+    const node = new ListNode(value);
+    if (this.#tail == null) {
+      this.#head = node;
+    } else {
+      this.#tail.next = node;
+      node.prev = this.#tail;
+    }
+    this.#tail = node;
+    this.#length++;
+    return this;
+  }
+
+  // TODO: tests
+  pop() {
+    if (this.#tail == null) return undefined;
+    const node = this.#tail;
+
+    if (this.length == 1) {
+      this.#head = null;
+      this.#tail = null;
+    } else {
+      this.#tail = node.prev;
+      this.#tail!.next = null;
+      node.prev = null;
+    }
+
+    this.#length--;
+    return node.value;
+  }
+
+  // WIP
+  // TODO: shift, unshift, _getNode, get, set, insert, remove, reverse, maybe other methods?
+}
