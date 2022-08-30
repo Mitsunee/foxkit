5555fb6bff042c45b7cd761c2a7973f765f5d7d7
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-08-30 15:43:15 +0200

    add(util): (un)shift, get(Node), set methods for List

--- packages/util/src/object/List.ts
+++ packages/util/src/object/List.ts
@@ -25,7 +25,6 @@ export class List<T> {
     return this.#length;
   }
 
-  // TODO: tests
   push(value: T) {
     const node = new ListNode(value);
     if (this.#tail == null) {
@@ -39,7 +38,6 @@ export class List<T> {
     return this;
   }
 
-  // TODO: tests
   pop() {
     if (this.#tail == null) return undefined;
     const node = this.#tail;
@@ -57,6 +55,66 @@ export class List<T> {
     return node.value;
   }
 
+  shift() {
+    if (this.#head == null) return undefined;
+    const node = this.#head;
+    if (node.next == null) {
+      this.#tail = null;
+    }
+    this.#head = node.next;
+    this.#length--;
+    return node.value;
+  }
+
+  unshift(value: T) {
+    const node = new ListNode(value);
+    if (this.#head) {
+      node.next = this.#head;
+      this.#head.prev = node;
+    } else {
+      this.#tail = node;
+    }
+    this.#head = node;
+    this.#length++;
+    return this;
+  }
+
+  getNode(n: number) {
+    if (n < 0 || n >= this.#length) return undefined;
+    const mid = this.#length / 2;
+    let curr: ListNode<T>;
+    if (n < mid) {
+      curr = this.#head!;
+      for (let i = 0; i < n; i++) {
+        curr = curr.next!;
+      }
+    } else {
+      curr = this.#tail!;
+      for (let i = this.#length - 1; i > n; i--) {
+        curr = curr.prev!;
+      }
+    }
+    return curr;
+  }
+
+  get(n: number) {
+    const node = this.getNode(n);
+    if (node) return node.value;
+    return undefined;
+  }
+
+  set(n: number, value: T) {
+    if (n == this.#length) {
+      this.push(value);
+      return true;
+    }
+
+    const node = this.getNode(n);
+    if (!node) return false;
+    node.value = value;
+    return true;
+  }
+
   // WIP
-  // TODO: shift, unshift, _getNode, get, set, insert, remove, reverse, maybe other methods?
+  // TODO: insert, remove, ...array methods, toArray, fromArray, maybe other methods?
 }
--- packages/util/tests/object.List.test.js
+++ packages/util/tests/object.List.test.js
@@ -2,11 +2,11 @@ import { test } from "uvu";
 import * as assert from "uvu/assert";
 import { List } from "../src/object/List.ts";
 
-test("Can push elements to List", () => {
+test("Can push elements into List", () => {
   const list = new List();
 
   // push first element
-  list.push("lorem");
+  assert.is(list.push("lorem"), list, "return this");
   assert.is(list.head, "lorem", "updated head");
   assert.is(list.tail, "lorem", "updated tail");
   assert.is(list.length, 1, "updated length");
@@ -33,6 +33,151 @@ test("Can pop elements from List", () => {
   assert.is(list.head, undefined, "cleared head");
   assert.is(list.tail, undefined, "cleared tail");
   assert.is(list.length, 0, "updated length");
+
+  // empty list
+  assert.is(list.pop(), undefined, "return undefined for empty list");
+});
+
+test("Can shift elements from List", () => {
+  const list = new List();
+  list.push("lorem").push("ipsum").push("dolor");
+
+  // shift once
+  assert.is(list.shift(), "lorem", "shifted head");
+  assert.is(list.head, "ipsum", "updated head");
+  assert.is(list.length, 2, "updated length");
+
+  // shift entire list
+  list.shift();
+  list.shift();
+  assert.is(list.head, undefined, "cleared head");
+  assert.is(list.tail, undefined, "cleared tail");
+  assert.is(list.length, 0, "updated length");
+
+  // empty list
+  assert.is(list.shift(), undefined, "return undefined for empty list");
+});
+
+test("Can unshift elements into List", () => {
+  const list = new List();
+
+  // unshift first element
+  assert.is(list.unshift("dolor"), list, "return this");
+  assert.is(list.head, "dolor", "updated head");
+  assert.is(list.tail, "dolor", "updated tail");
+  assert.is(list.length, 1, "updated length");
+
+  // unshift more
+  list.unshift("ipsum").unshift("lorem");
+  assert.is(list.head, "lorem", "updated head");
+  assert.is(list.tail, "dolor", "tail unchanged");
+  assert.is(list.length, 3, "updated length");
+});
+
+test("Can get ListNode by index", () => {
+  const list = new List();
+  const lorem = [
+    "lorem", // 0
+    "ipsum", // 1
+    "dolor", // 2
+    "sit", // 3
+    "amet", // 4
+    "consectetur", // 5
+    "adipisicing" // 6
+  ]; // length: 7
+
+  // empty list
+  assert.is(list.getNode(0), undefined, "return undefined for empty list");
+
+  // fill list
+  for (const item of lorem) list.push(item);
+
+  // number out of range
+  assert.is(
+    list.getNode(-1),
+    undefined,
+    "reject negative number with undefined"
+  );
+  assert.is(
+    list.getNode(7),
+    undefined,
+    "reject number greater or equal to length with undefined"
+  );
+  assert.is(
+    list.getNode(8),
+    undefined,
+    "reject number greater or equal to length with undefined"
+  );
+
+  // correct values
+  for (let i = 0; i < lorem.length; i++) {
+    assert.is(
+      list.getNode(i)?.value,
+      lorem[i],
+      `get Node from list for index ${i}`
+    );
+  }
+});
+
+test("Can get value by index", () => {
+  const list = new List();
+  const lorem = [
+    "lorem", // 0
+    "ipsum", // 1
+    "dolor", // 2
+    "sit", // 3
+    "amet", // 4
+    "consectetur", // 5
+    "adipisicing" // 6
+  ]; // length: 7
+
+  // empty list
+  assert.is(list.get(0), undefined, "return undefined for empty list");
+
+  // fill list
+  for (const item of lorem) list.push(item);
+
+  // number out of range
+  assert.is(list.get(-1), undefined, "reject negative number with undefined");
+  assert.is(
+    list.get(7),
+    undefined,
+    "reject number greater or equal to length with undefined"
+  );
+  assert.is(
+    list.get(8),
+    undefined,
+    "reject number greater or equal to length with undefined"
+  );
+
+  // correct values
+  for (let i = 0; i < lorem.length; i++) {
+    assert.is(list.get(i), lorem[i], `get value from list for index ${i}`);
+  }
+});
+
+test("Can set value by index", () => {
+  const list = new List();
+  list.push("lorem").push("ipsum").push("dolor");
+
+  // invalid index
+  assert.not(list.set(-1, "foobar"), "reject negative number with false");
+  assert.not(
+    list.set(4, "foobar"),
+    "reject number greater than length with false"
+  );
+
+  // set index 1 to foobar
+  assert.ok(list.set(1, "foobar"), "don't reject good index");
+  assert.is(list.get(1), "foobar", "set value");
+
+  // set index equal to length (aka push)
+  assert.ok(
+    list.set(list.length, "barbaz"),
+    "don't reject index equal to length"
+  );
+  assert.is(list.tail, "barbaz", "correctly pushed element");
+  assert.is(list.length, 4, "correctly pushed element");
 });
 
 test.run();
